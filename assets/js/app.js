efd51ad410b37a04f32b0786579ccb6211c6aa0e
Tyler Smith <tylersmith.me@gmail.com>
2016-09-01 20:05:47 -0700

    stash

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,20 @@
-"use strict";
+$(function () {
+  "use strict";
 
-$(function(){
-  // Create App objct
+  // Create App object
   var App = {
+    // Exported methods
     createvps: createvps,
 
-    debug: /debug/i.test(window.location.search),
+    // Data
+    devmode: /devmode/i.test(window.location.search),
     dropletId: null,
     droplet: null,
-    hasSSHKeyInstalled: false,
+    hasSSHKeyInstalled: false
   };
 
-  window.App = App
+  // Export App
+  window.App = App;
 
   //
   // MiniProvistor client-side application logic begins
@@ -20,202 +23,229 @@ $(function(){
   //
   // Constants
   //
-  const dropletSizes = ['512mb', '1gb', '2gb'];
-  const dropletRegions = ['nyc1', 'nyc2', 'nyc3', 'sfo1', 'sfo2'];
+  var dropletSizes = ['512mb', '1gb', '2gb'];
+  var dropletRegions = ['nyc1', 'nyc2', 'nyc3', 'sfo1', 'sfo2'];
 
-  const sshPubKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUNtHUhGE63rkQdpvITmtGVEjziKWxaoKknAFDhsWvaR4PqFguqGd90BlYlMJM11jwrnx1mY5bcWGhjGdg6K+Equw0mynGJdlKOUFD30FohPdhVUkeW5J0fzzR7WB8uV4NlG41TBCYIUsljbmC/jGNTQ/VV1mmL5KlVejnNSAkeC/bPZmu2RAK8WOkj7iZ+MfPfYZDlqJMsYHaQa6n9q7ah0ofzxolgXLcJTBPfBt6TvDGbGdYjNYD7EkGkclpPLyNII6U+4yluF6I2y0dVmVte8rplrNbxBgcz6+8I1uwzGUmEDMhtAu+SD65vB7qDpS9rxUq3MIKpiVvk8rhd+Q3 MiniProvistor";
-  const sshPubKeyFingerprint = "77:b7:5b:68:de:ce:37:bd:f7:a4:95:dc:b2:b3:41:fa";
+  var sshPubKey =
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUNtHUhGE63rkQdpvITmtGVEjziKWxaoKknAFDhsWvaR4PqFguqGd90BlYlMJM11jwrnx1mY5bcWGhjGdg6K+Equw0mynGJdlKOUFD30FohPdhVUkeW5J0fzzR7WB8uV4NlG41TBCYIUsljbmC/jGNTQ/VV1mmL5KlVejnNSAkeC/bPZmu2RAK8WOkj7iZ+MfPfYZDlqJMsYHaQa6n9q7ah0ofzxolgXLcJTBPfBt6TvDGbGdYjNYD7EkGkclpPLyNII6U+4yluF6I2y0dVmVte8rplrNbxBgcz6+8I1uwzGUmEDMhtAu+SD65vB7qDpS9rxUq3MIKpiVvk8rhd+Q3 MiniProvistor";
+  var sshPubKeyFingerprint =
+    "77:b7:5b:68:de:ce:37:bd:f7:a4:95:dc:b2:b3:41:fa";
+
+  var getDropletIPMaxAttempts = 60;
+  var getDropletIPPollInterval = 1000;
+
+  // create vps creates and sets up a droplet for production OpenBazaar use
+  function createvps(caller) {
+    // Get the form calling this method
+    var $form = $(caller).parents("form");
+
+    // Get the required inputs from the form and validate them as much as we can
+    var inputs = getInputsFromForm($form);
+    validateInputs(inputs);
+
+    // Perform the provisioning
+    provisionOpenBazaarDroplet(inputs.token, {
+        // Append utc epoch in milliseconds to name for uniqueness
+        name: "obdroplet-" + (new Date().getTime()),
+
+        // Add the MiniProvistor fingerprint
+        ssh_keys: [sshPubKeyFingerprint],
+
+        // Add settings
+        region: inputs.region,
+        size: inputs.size,
+
+        // Use ubuntu 16.04 LTS
+        image: "ubuntu-16-04-x64",
+
+        // No frills; users can opt-in later
+        backups: false,
+        ipv6: false,
+        user_data: null,
+        private_networking: null,
+        volumes: null
+      })
+      .done(function () {
+        $("#dasinfo").html("OpenBazaar Installed");
+      })
+      .fail(function (err) {
+        handleError(err);
+      });
+  }
 
   //
   // Private methods
   //
 
-  // create vps creates and sets up a droplet for production OpenBazaar use
-  function createvps() {
-  	// Get inputs
-  	var token = $("#token").val();
-  	var size = $("#size").val();
-  	var region = $("#region").val();
+  // handleError logs the error and shows it to the  user
+  function handleError(error) {
+    console.log("error creating droplet");
+    $('#dasinfo').text("error creating droplet");
+    return false;
+  }
 
-  	// Validate inputs
-  	if (dropletSizes.indexOf(size) === -1) {
-  		handleError("Invalid droptlet size");
-  		return
-  	}
+  // getInputsFromForm returns an object with the required inputs for node creation
+  function getInputsFromForm($form) {
+    return {
+      token: $form.find("#token").val(),
+      size: $form.find("#size").val(),
+      region: $form.find("#region").val()
+    }
+  }
 
-  	if (dropletRegions.indexOf(region) === -1) {
-  		handleError("Invalid droptlet region");
-  		return
-  	}
+  // validateInputs validates the data from the form as much as we can
+  function validateInputs(inputs) {
+    // Basic token validation
+    if (inputs.token.length != 64) {
+      handleError("Token should be 64 characters");
+      return
+    }
 
-    provisionOpenBazaarDroplet(token, {
-      // Append utc epoch in milliseconds to name for uniqueness
-      name: "obdroplet-" + (new Date().getTime()),
+    // Validate the size
+    if (dropletSizes.indexOf(inputs.size) === -1) {
+      handleError("Invalid droptlet size");
+      return;
+    }
 
-      // Add the MiniProvistor fingerprint
-      ssh_keys: [sshPubKeyFingerprint],
-
-      // Add settings
-      region: region,
-      size: size,
-
-      // Use ubuntu 16.04 LTS
-      image: "ubuntu-16-04-x64",
-
-      // No frills; users can opt-in later
-      backups: false,
-      ipv6: false,
-      user_data: null,
-      private_networking: null,
-      volumes: null,
-    });
+    // Validate the region
+    if (dropletRegions.indexOf(inputs.region) === -1) {
+      handleError("Invalid droptlet region");
+      return
+    }
   }
 
   // provisionOpenBazaarDroplet creates a production ready OpenBazaar droplet
   // for the given token and data
   function provisionOpenBazaarDroplet(token, dropletData) {
     // Debugging info
-    if (App.debug) {
+    if (App.devmode) {
       console.log("Token: " + token);
       console.log("Droplet Config: " + JSON.stringify(dropletData));
     }
 
-    var deferred = $.Deferred();
-    deferred
-      .then(function(){ return ensureSSHKeyExists(token, sshPubKeyFingerprint) })
-      .then(function(){ return createDroplet(token, dropletData); })
-      .then(function(data){ return getDropletData(token, data.droplet.id); })
-      .then(function(data){ return setupDroplet(data.droplet.networks.v4[0].ip_address); })
-      .fail(function(err){ handleError(err); })
-      .done(function(){ $("#dasinfo").html("OpenBazaar Installed"); })
-    deferred.resolve();
-  }
+    var doClient = new DigitalOcean(token);
 
-  // handleError logs the error shows the user
-  function handleError(error) {
-  	console.log("error creating droplet");
-  	$('#dasinfo').text("error creating droplet");
-    return false;
+    // Return a promise to fullfil a long chain of commands to build a new droplet
+    // Start with making sure we have the MiniProvistor ssh public key installed
+    return ensureSSHKeyExists(doClient, sshPubKeyFingerprint)
+
+    // Create the droplet
+    .then(function () {
+      return doClient.createDroplet(dropletData);
+    })
+
+    // Wait for the droplet to be created
+    .then(function (data) {
+      return waitForCreation(doClient, data.droplet.id);
+    })
+
+    // Install OpenBazaar on the droplet
+    .then(function (data) {
+      return setupDroplet(data.droplet.networks.v4[0].ip_address);
+    })
+
+    // Promise the caller we'll do our best to do all this
+    .promise()
   }
 
   // ensureSSHKeyExists returns true or false dpending on whether or not that account
   // owning the token has the given ssh key fingerprint registered
-  function ensureSSHKeyExists(token, fingerprint) {
-    return $.Deferred(function(defer){
-      if (App.hasSSHKeyInstalled) {
-        defer.resolve();
-        return
-      }
+  function ensureSSHKeyExists(doClient, fingerprint) {
+    var deferred = $.Deferred();
 
-      $.ajax({
-    		url: "https://api.digitalocean.com/v2/account/keys/" + fingerprint,
-    		beforeSend: function(xhr) {
-    			xhr.setRequestHeader("Authorization", "BEARER " + token );
-    		},
-    		type: 'GET',
-    		dataType: 'json',
-    		contentType: 'application/json',
-    		success: function(){
-          App.hasSSHKeyInstalled = true
-          defer.resolve.apply(this, arguments);
-        },
-        error: function(err) {
-          if (err.status === 404) {
-            App.hasSSHKeyInstalled = false
+    // If we know we have the key installed then we're good to go
+    if (App.hasSSHKeyInstalled) {
+      return deferred.resolve().promise();
+    }
 
-            installSSHKey(token, sshPubKey)
-              .done(function(){
-                App.hasSSHKeyInstalled = true
-                defer.resolve.apply(this, arguments)
-              })
-              .fail(function(){ defer.reject.apply(this, arguments); })
-            return err;
-          }
-
-          defer.reject.apply(this, arguments)
-          return err
-        }
+    // Get the key by fingerprint. If it doesn't exist create it.
+    doClient.getSSHKeyByFingerprint(fingerprint)
+      .done(function () {
+        App.hasSSHKeyInstalled = true;
+        return deferred.resolve.apply(this, arguments);
       })
-    }).promise();
+      .fail(function (err) {
+        // Key doesn't exist so we'll create it
+        if (err.status === 404) {
+          App.hasSSHKeyInstalled = false
+
+          // Create the key
+          return doClient.createSSHKey("MiniProvistor SSH Key", sshPubKey)
+            .done(function () {
+              App.hasSSHKeyInstalled = true
+              return deferred.resolve.apply(this, arguments)
+            })
+            .fail(function () {
+              return deferred.reject.apply(this, arguments);
+            });
+        }
+
+        // It was a real error, fail
+        return deferred.reject.apply(this, arguments)
+      });
+
+    // Return a promise for the check/create actions
+    return deferred.promise();
   }
 
   // installSSHKey creates the given key on digitalocean.
   // On success calls callback. On error calls errorback.
   // An error response containing "already in use" is treated as a success.
-  function installSSHKey(token, key) {
-  	return $.ajax({
-  		url: "https://api.digitalocean.com/v2/account/keys",
-  		beforeSend: function(xhr) {
-  			xhr.setRequestHeader("Authorization", "BEARER " + token );
-  		},
-  		type: 'POST',
-  		dataType: 'json',
-  		contentType: 'application/json',
-  		processData: true,
-  		data: JSON.stringify({
-  			name: "MiniProvistor SSH Key",
-  			public_key: sshPubKey,
-  		})
-  	});
-  }
+  // function installSSHKey(doClient, key) {
+  //   return doClient.createSSHKey("MiniProvistor SSH Key", key);
+  // }
 
   // createDroplet creats a droplet. It makes a 2nd request to obtain the ip.
-  function createDroplet(token, dropletData) {
-  	return $.ajax({
-  		url: "https://api.digitalocean.com/v2/droplets",
-  		beforeSend: function(xhr) {
-  			xhr.setRequestHeader("Authorization", "BEARER " + token );
-  		},
-  		type: 'POST',
-  		dataType: 'json',
-  		contentType: 'application/json',
-  		processData: true,
-  		data: JSON.stringify(dropletData)
-  	}).promise();
-  }
+  // function createDroplet(doClient, dropletData) {
+  //   return doClient.createDroplet(dropletData);
+  // }
 
-  function getDropletData(token, dropletId) {
-    var deferred = new Deferred()
-    setTimeout(function(){ deferred.resolve(); }, 1000);
-    return deferred;
-//     function sleep (time) {
-//   return new Promise((resolve) => setTimeout(resolve, time));
-// }
-//
-// // Usage!
-// sleep(500).then(() => {
-//     // Do something after the sleep!
-// })
+  // waitForCreation polls the api X times trying to get the ip
+  function waitForCreation(doClient, dropletId) {
+    var deferred = $.Deferred();
+    var attempts = 0;
 
-    return $.ajax({
-      url: "https://api.digitalocean.com/v2/droplets/" + dropletId,
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("Authorization", "BEARER " + token );
-      },
-      dataType: 'json'
-    }).promise();
+    // poll gets droplet data and if an ipv4 exists we stop, otherwise keep going
+    // until the configured stopping point
+    function poll() {
+      doClient.getDroplet(dropletId)
+        .done(function (data) {
+          if (data.droplet.networks.v4.length) {
+            deferred.resolve(data);
+            return data;
+          }
+
+          if (attempts >= getDropletIPMaxAttempts) {
+            deferred.reject(data);
+          }
+
+          attempts++
+          setTimeout(poll, getDropletIPPollInterval);
+        })
+        .fail(function () {
+          defer.reject.apply(this, arguments);
+        });
+    }
+
+    // Start polling
+    poll();
+
+    // Return a promise to try really hard or fail
+    return deferred.promise();
   }
 
   // setupDroplet turns the bare droplet into a production OpenBazaar server
-  function setupDroplet(ipaddress) {
-  	return $.ajax({
-  		url: "http://localhost:8080/api/v1/provision",
-  		type: 'POST',
-  		dataType: 'json',
-  		contentType: 'application/json',
-  		processData: true,
-  		data: JSON.stringify({
-  			ipaddress: ipaddress,
-  		}),
-
-      // success: function () {
-  		// 	return callbacks.success();
-  		// },
-      //
-  		// error: function(){
-  		// 	return callbacks.error();
-  		// }
-  	});
+  function setupDroplet(ip_address) {
+    return $.ajax({
+      url: "http://localhost:8080/api/v1/provision",
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+      processData: true,
+      data: JSON.stringify({
+        ip_address: ip_address,
+      })
+    }).promise();
   }
 
 
@@ -224,38 +254,38 @@ $(function(){
   //
 
   function testvps() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('testvps_ipaddress').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('testvps_ipaddress').value = ipaddress;
   }
 
   function install() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('install_ipaddress').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('install_ipaddress').value = ipaddress;
   }
 
   function scoop3() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('theipaddress3').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('theipaddress3').value = ipaddress;
   }
 
   function start() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('start_ipaddress').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('start_ipaddress').value = ipaddress;
   }
 
   function stop() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('stop_ipaddress').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('stop_ipaddress').value = ipaddress;
   }
 
   function update() {
-  	var ipaddress = document.getElementById('ipaddress').value;
-  	console.log(ipaddress);
-  	document.getElementById('update_ipaddress').value = ipaddress;
+    var ipaddress = document.getElementById('ipaddress').value;
+    console.log(ipaddress);
+    document.getElementById('update_ipaddress').value = ipaddress;
   }
 });
---
+++ assets/js/digitalocean.js
@@ -0,0 +1,81 @@
+$(function () {
+  "use strict";
+
+  // Create DigitalOcean object
+  function DO(token) {
+    this._token = token;
+    this._root = "https://api.digitalocean.com/v2/"
+  };
+
+  // Export
+  window.DigitalOcean = DO;
+
+  //
+  // Public methods
+  //
+
+  //
+  // SSH Key methods
+  //
+  DO.prototype.getSSHKeyByFingerprint = function (fingerprint) {
+    return this._request("GET", "account/keys/" + fingerprint);
+  };
+
+
+  DO.prototype.createSSHKey = function (name, key) {
+    return this._request("POST", "account/keys", {
+      data: JSON.stringify({
+        name: name,
+        public_key: key,
+      })
+    });
+  }
+
+  //
+  // Droplet methods
+  //
+  DO.prototype.createDroplet = function (dropletData) {
+    return this._request("POST", "droplets", {
+      data: JSON.stringify(dropletData)
+    });
+  }
+
+  DO.prototype.getDroplet = function (id) {
+    return this._request("GET", "droplets/" + id);
+  }
+
+
+  //
+  // Private methods
+  //
+
+  DO.prototype._request = function (method, path, reqData) {
+    reqData = reqData || {};
+
+    // Set the HTTP verb for the req
+    reqData.type = method;
+
+    // Set the token authorization header
+    var token = this._token;
+    reqData.beforeSend = function (xhr) {
+      xhr.setRequestHeader("Authorization", "BEARER " + token);
+    }
+
+    // Set headders for sending and receiving JSON data
+    if (!reqData.dataType) {
+      reqData.dataType = "json";
+    }
+
+    if (!reqData.contentType) {
+      reqData.contentType = "application/json";
+    }
+
+    // Build the url
+    if (!reqData.url) {
+      reqData.url = this._root + path;
+    }
+
+    // Return a promise for the request
+    return $.ajax(reqData).promise();
+  }
+});
