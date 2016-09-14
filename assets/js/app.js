0d1f20c750fc8c67078c6ad0e73a2e0e9f5a7355
Tyler Smith <tylersmith.me@gmail.com>
2016-09-13 22:20:01 -0700

    Update installation script for ob-relay.

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,12 @@ $(function () {
   // MiniProvistor client-side application logic begins
   //
 
-  var getDropletIPMaxAttempts = 60;
-  var getDropletIPPollInterval = 5000;
+  // Set limits on how fast/much we poll for a new droplet to be active
+  // Try every 5 seconds for 10 minutes
+  var getDropletStateMaxAttempts = 120;
+  var getDropletStatePollInterval = 5000;
 
-  // create vps creates and sets up a droplet for production OpenBazaar use
+  // Create vps creates and sets up a droplet for production OpenBazaar use
   function createvps(caller, cloudInitScriptTemplate) {
 
     $("#dasinfo").html("<code>Working... please wait. Your droplet and OpenBazaar node details will be show below in 2-3 minutes.</code>");
@@ -23,50 +25,50 @@ $(function () {
     var inputs = getInputsFromForm($form);
 
     // Generate passwords
-    var vps_password = bip39.generateMnemonic();
-    var ob_password = bip39.generateMnemonic();
+    var vpsPassword = bip39.generateMnemonic();
+    var obPassword = bip39.generateMnemonic();
+    var sessionSecret = bip39.generateMnemonic(256);
 
     // Create a DO client
     var doClient = new DigitalOcean(inputs.token);
 
+    // Create the cloud init script from the template
+    var cloudInitScript = cloudInitScriptTemplate
+      .replace("{{vpsPassword}}", vpsPassword)
+      .replace("{{obPassword}}", obPassword)
+      .replace("{{sessionSecret}}", sessionSecret)
+
     // Perform the provisioning
     doClient.createDroplet({
-        // Append utc epoch in milliseconds to name for uniqueness
-        name: "obdroplet-" + (new Date().getTime()),
+      size: "512mb",
+      region: "sfo1",
+      image: "ubuntu-14-04-x64",
+      name: "obdroplet-" + (new Date().getTime()),
+      user_data: cloudInitScript
+    })
 
-        // Add settings
-        region: "sfo1",
-        size: "512mb",
+    // After creating the droplet we need to wait for it to be active
+    .then(function (data) {
+      return waitForCreation(doClient, data.droplet.id);
+    })
 
-        // Use ubuntu 14.04 LTS
-        image: "ubuntu-14-04-x64",
+    // Upon success show a message to the user with their details
+    .done(function (data) {
+      $("#dasinfo").html("Your Digital Ocean droplet was created and can be found at <kbd>" + data.droplet.networks
+        .v4[0].ip_address + "</kbd>. OpenBazaar is now installing.</br></br><u>To login to your droplet via SSH:</u></br>Droplet username: <code>openbazaar</code></br>Droplet password: <code>" + vpsPassword + "</code></br></br>The OpenBazaar node is installing on your droplet and should be ready in <strong>5-7 minutes</strong>.</br></br><u>To login to your OpenBazaar node:</u></br>Username: <code>admin</code></br>OB password: <code>" + obPassword + "</code></br></br><strong>Save these details immediately!</strong>");
+    })
 
-        user_data: Mustache.render(cloudInitScriptTemplate, {
-          vps_password: vps_password,
-          ob_password: ob_password,
-        }),
+    // Show error message upon failure
+    .fail(function (err) {
+      handleError(err);
 
-        // No frills; users can opt-in later
-        backups: false,
-        ipv6: false,
-        private_networking: null,
-        volumes: null
-      }).then(function (data) {
-        return waitForCreation(doClient, data.droplet.id);
-      })
-      .done(function (data) {
-        $("#dasinfo").html("Your Digital Ocean droplet was created and can be found at <kbd>" + data.droplet.networks
-          .v4[0].ip_address + "</kbd>. OpenBazaar is now installing.</br></br><u>To login to your droplet via SSH:</u></br>Droplet username: <code>openbazaar</code></br>Droplet password: <code>" + vps_password + "</code></br></br>The OpenBazaar node is installing on your droplet and should be ready in <strong>5-7 minutes</strong>.</br></br><u>To login to your OpenBazaar node:</u></br>Username: <code>admin</code></br>OB password: <code>" + ob_password + "</code></br></br><strong>Save these details immediately!</strong>");
-      })
-      .fail(function (err) {
-        handleError(err);
-        if (JSON.stringify(err.status) == 401) {
-          $('#dasinfo').html("<code>" + JSON.stringify(err.responseJSON.message) + "</code></br></br><code>Please check that your API token is correct.</code>");
-        }
-        else {
-          $('#dasinfo').html("<code>" + JSON.stringify(err.responseJSON.message) + "</code>.");
-        }
-      });
+      // A 401 most likely means we have an invalid API token
+      if (JSON.stringify(err.status) == 401) {
+        $('#dasinfo').html("<code>" + JSON.stringify(err.responseJSON.message) + "</code></br></br><code>Please check that your API token is correct.</code>");
+      } else {
+        $('#dasinfo').html("<code>" + JSON.stringify(err.responseJSON.message) + "</code>.");
+      }
+    });
   }
 
   //
@@ -76,23 +78,22 @@ $(function () {
   // handleError logs the error and shows it to the  user
   function handleError(error) {
     console.log("error creating droplet");
-    /*$('#dasinfo').text(err);*/
     return false;
   }
 
   // getInputsFromForm returns an object with the required inputs for node creation
   function getInputsFromForm($form) {
     return {
-      token: $form.find("#token").val(),
-      size: $form.find("#size").val(),
-      region: $form.find("#region").val()
+      token: $form.find("input[name=token]").val(),
+      size: $form.find("input[name=size]").val(),
+      region: $form.find("input[name=region]").val()
     }
   }
 
   // waitForCreation polls the api X times trying to get the ip
   function waitForCreation(doClient, dropletId) {
-    var deferred = $.Deferred();
-    var attempts = 0;
+    var deferred = $.Deferred(),
+      attempts = 0;
 
     // poll gets droplet data and if an ipv4 exists we stop, otherwise keep going
     // until the configured stopping point
@@ -105,12 +106,12 @@ $(function () {
             return data;
           }
 
-          if (attempts >= getDropletIPMaxAttempts) {
+          if (attempts >= getDropletStateMaxAttempts) {
             deferred.reject(data);
           }
 
           attempts++
-          setTimeout(poll, getDropletIPPollInterval);
+          setTimeout(poll, getDropletStatePollInterval);
         })
         .fail(function () {
           defer.reject.apply(this, arguments);
