d1c899a338bc915dd97abb2d6e6fc7c4ee91d5d1
Yusef Napora <yusef@napora.org>
2017-02-22 14:48:43 -0500

    install gcc-5 for updated libstdc++ dependency

--- src/install.sh
+++ src/install.sh
@@ -132,6 +132,12 @@ _mkdir /home/mediachain/logs
 
 setState INSTALLING_SYSTEM_PACKAGES
 
+# install gcc-5 so we can get libc6 (required by recent concat versions / rocksdb)
+apt-get install -y software-properties-common
+apt-add-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y gcc-5 g++-5
+
 # Update packages and do basic security hardening
 apt-get upgrade -y
 
